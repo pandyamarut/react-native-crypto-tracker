d886c1fa5b32ea3919c4c46f6cc539bb66fd7722
user <pandyamarut@gmail.com>
2020-01-09 18:47:12 +0530

    chapter-01.

--- react-native-redux-crypto-tracker/App.js
+++ react-native-redux-crypto-tracker/App.js
@@ -1,19 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from './src/components';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on you app!</Text>
-    </View>
-  );
+
+export default class App extends Component {
+  render(){
+    return (
+      <View>
+        <Header/>
+      </View>
+
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
